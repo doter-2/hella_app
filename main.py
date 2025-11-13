de99e288fb3fedd94e88780e4b3c9803e376ea91
Your Name <you@example.com>
2025-11-13 17:02:44 +0600

    changed

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from hello!")
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+my_variable = os.getenv('MY_KEY')
+print(my_variable)
+
+def main():
+    print("Hi gitler ")
+
+name = "Tom"
 
 if __name__ == "__main__":
     main()
